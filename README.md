2d062901a28374844d81933701f67c27c724e011
Pythonic-Rainbow <wongchingho0@gmail.com>
2019-01-20 01:31:59 +0800

    New project setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Ocean
+
+This UEFI project is based on TianoCore UDK 2018. The `Ocean` module is in `edk2-vUDK2018\Ocean`
---
+++ edksetup.bat
@@ -0,0 +1,4 @@
+@echo off
+REM set your PYTHON_HOME accordingly , MUST BE VERSION 2.7
+set PYTHON_HOME=D:\Python\2.7
+edk2-VUDK2018/edksetup.bat
